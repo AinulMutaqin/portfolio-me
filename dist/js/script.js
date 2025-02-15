23edbe7ae310acc155a03a5f73edc2c01b9a6c71
ainul.mutaqin <ainul.mutaqin@dentsu.com>
2025-02-15 21:51:12 +0700

    Enhancement dark mode, up button & scroll smooth

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,14 +7,47 @@ hamburger.addEventListener('click', function() {
     navMenu.classList.toggle('hidden');
 });
 
+// click outside hamburger button
+window.addEventListener('click', function(e) {
+    if(e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+});
+
 // Handling Navbar Fixed
-window.onload = function() {
+window.onscroll = function() {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
-
+    const toTop = document.querySelector('#to-top');
     if(window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
+}
+
+// Darkmode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function () {
+  if (darkToggle.checked) {
+    html.classList.add('dark');
+    localStorage.theme = 'dark';
+  } else {
+    html.classList.remove('dark');
+    localStorage.theme = 'light';
+  }
+});
+
+// handling toggle position suitable to mode
+if(localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class',
   theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
     colors: {
       primary: '#0ea5e9',
       secondary: '#64748b',
@@ -9,6 +14,8 @@ module.exports = {
       fortiary: '#334155',
       firthtiary: '#14b8a6',
       sixtiary: '#cbd5e1',
+      seventiary: '#1e293b',
+      eightiary: '#475569',
       dark: '#0f172a',
       white: '#f8fafc',
     },
